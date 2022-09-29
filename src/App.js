71ac65d3c5067220f1d2902b71bdef55a9a8c8f6
areumYang <yangareum1818@nate.com>
2022-09-29 20:11:40 +0900

    useReducer를 이용한 은행 예금출금 만들기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+import { BANKACTION_TYPES, bankReducer } from "./reducer/simpleReducer";
+import TodoListItem from "./todoList";
+import "./App.css";
+
+// reducer : state를 업데이트, 기록 해주는 역할 (은행)
+// dispatch : state 업데이트를 위한 요구사항
+// action : 요구의 내용
+// [새로운state, dispatch] = useReducer(리듀서 , 초기값(함수로 만든 객체, 직관적인 값인 0 등..이 들어갈 수 있다.) )
+
+const TODOACTION_TYPES = {
+  addlist: "add-todo",
+  removelist: "remove-todo",
+};
+
+const todoReducer = (state, action) => {
+  console.log("todolist", state, action);
+
+  switch (action.type) {
+    case TODOACTION_TYPES.addlist:
+      return;
+  }
+};
+
+const initialstate = {
+  count: 0,
+  lists: [
+    {
+      id: Date.now(),
+      name: "Eille",
+      isHere: false,
+    },
+  ],
+};
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [name, setName] = useState("");
+
+  const [money, bankDispatch] = useReducer(bankReducer, 0);
+  const [todolistInfo, todoDispatch] = useReducer(todoReducer, initialstate);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrap">
+        <h2>간단한 useReducer 은행</h2>
+        <p>잔고 : {money} 원</p>
+
+        <div className="clickaction">
+          <input
+            type="number"
+            value={number}
+            onChange={(e) => setNumber(parseInt(e.target.value))}
+            step="1000"
+          />
+          <button
+            onClick={() => {
+              bankDispatch({ type: BANKACTION_TYPES.deposit, payload: number });
+            }}
+          >
+            예금 +
+          </button>
+          <button
+            onClick={() => {
+              bankDispatch({
+                type: BANKACTION_TYPES.withdraw,
+                payload: number,
+              });
+            }}
+          >
+            출금 -
+          </button>
+        </div>
+      </div>
+
+      <div className="wrap">
+        <h2>TODO LIST</h2>
+        <p>LIST의 갯수 : {todolistInfo.count}</p>
+        <div className="clickaction">
+          <input
+            type="text"
+            placeholder="LIST를 추가해주세요."
+            value={name}
+            onChang={(e) => setName(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              todoDispatch({ type: "add-todo", payload: { name } });
+            }}
+          >
+            LIST 추가
+          </button>
+
+          {todolistInfo.lists.map((list) => {
+            <TodoListItem key={list.id} name={list.name} />;
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/todoList.js
@@ -0,0 +1,9 @@
+const TodoListItem = ({ name }) => {
+  return (
+    <div>
+      <span>{name}</span>
+      <button></button>
+    </div>
+  );
+};
+export default TodoListItem;
